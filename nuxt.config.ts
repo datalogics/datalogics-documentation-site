eb10b3820dd046444b04b7571f8410f7b1a7c1e3
ry-jp <ryanp@datalogics.com>
2024-10-08 11:24:08 -0500

    remove base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,6 @@ export default defineNuxtConfig({
     join(currentDir, './assets/css/themes.css'),
   ],
   content: {
-    base: '/documentation/',
     documentDriven: true,
     highlight: {
       theme: {
